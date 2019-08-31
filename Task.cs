4681d547a4b68420fdadc8aa51641c3b65e4ce95
Dana Simmons <dsanalog@gmail.com>
2019-08-31 09:46:23 -0400

    Add entity and SQLite modules
    dotnet add package Microsoft.EntityFrameworkCore.Design
    dotnet add Microsoft.EntityFrameworkCore.Sqlite

--- Task.cs
+++ Task.cs
@@ -6,15 +6,7 @@ namespace CSTodoApp
 {
     public class Task
     { 
-        public string Name;
-        public int Priority;
-        public bool Complete;
-        public Task(string name, int priority)
-        {
-            Name = name;
-            Priority = priority;
-            Complete = false;
-        }
+        
         
     }
 }
